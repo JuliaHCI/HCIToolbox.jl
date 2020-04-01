076cbcfacacbb24580071837c2f5356dfedf98e0
Siddharth <siddharthlal25@gmail.com>
2020-04-01 19:53:14 +0530

    added tests

--- src/HCIToolbox.jl
+++ src/HCIToolbox.jl
@@ -8,8 +8,10 @@ export rotate,
        expand,
        combine,
        mask_circle!,
-       mask_circle
-    
+       mask_circle,
+       mask_annulus!,
+       mask_annulus
+
 
 # Utilities for dealing with cubes like derotating and median combining
 include("morphology.jl")
