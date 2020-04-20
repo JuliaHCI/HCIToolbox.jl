eb394a3748d51c06d3f66eda3fb6c86c5b2caa5b
Miles Lucas <mdlucas@hawaii.edu>
2020-04-19 20:12:23 -1000

    update morphology to use `fill`

--- src/morphology.jl
+++ src/morphology.jl
@@ -5,11 +5,11 @@ using ImageTransformations: imrotate, center, Linear
 # Stacking routines
 
 """
-    collapse(cube, [angles]; method=median, deweight=true)
+    collapse(cube, [angles]; method=median, deweight=true, fill=0)
 
 Combine all the frames of a cube using `method`. If `angles` are provided, will use [`derotate`](@ref) before combining.
 
-If `deweight` is true, the method of Bottom et al. 2017 will be used in which the combined image will be the derotated weighted sum of the frames weighted by the temporal variance.
+If `deweight` is true, the method of Bottom et al. 2017 will be used in which the combined image will be the derotated weighted sum of the frames weighted by the temporal variance. `fill` will be passed to [`derotate`](@ref).
 
 # Examples
 ```jldoctest
@@ -33,50 +33,53 @@ julia> collapse(X)
  1.0  1.0  1.0
  1.0  1.0  1.0
 
-julia> collapse(X, [0, 90])
-1×3×3 Array{Float64,3}:
-[:, :, 1] =
- 1.0  1.0  1.0
+julia> collapse(X, [0, 90], fill=NaN)
+3×3 Array{Float64,2}:
+ NaN    1.0  NaN
+   1.0  1.0    1.0
+ NaN    1.0  NaN
 
-[:, :, 2] =
- 0.0  1.0  0.0
-
-[:, :, 3] =
- 1.0  1.0  1.0
 ```
 
 # See Also
 [`collapse!`](@ref)
 """
-function collapse(cube::AbstractArray{T,3}; method=median, kwargs...) where T
+function collapse(cube::AbstractArray{T,3}; method=median) where T
     return method(cube, dims = 1)[1, :, :]
 end
 
-collapse(cube::AbstractArray{T,3}, angles::AbstractVector; method=median, deweight=true) where T = collapse(cube, angles, Val(deweight); method=method)
-# bog standard collapsing
-collapse(cube::AbstractArray{T,3}, angles::AbstractVector, ::Val{false}; method=median) = collapse(derotate(cube, angles); method = method)
-collapse(cube::AbstractArray{T,3}, angles::AbstractVector, v::Val{true}; kwargs...) = collapse!(deepcopy(cube), angles, v; kwargs...)
+function collapse(cube::AbstractArray{T,3}, angles::AbstractVector; method=median, deweight=true, fill=0) where T
+    return deweight ? _collapse_deweighted(cube, angles, fill=fill) :
+                      collapse(derotate(cube, angles, fill=fill); method = method)
+end
 
 """
     collapse!(cube, angles; method=median, deweight=true)
 
 An in-place version of the derotating [`collapse`](@ref). The only difference is in this version the cube will be derotated in-place.
 """
-collapse!(cube::AbstractArray{T,3}, angles::AbstractVector; method=median, deweight=true) where T = collapse!(cube, angles, Val(deweight); method=method)
-collapse!(cube::AbstractArray{T,3}, angles::AbstractVector, ::Val{false}; method=median) = collapse(derotate!(cube, angles); method = method)
+function collapse!(cube::AbstractArray{T,3}, angles::AbstractVector; method=median, deweight=true, fill=0) where T
+    return deweight ? _collapse_deweighted!(cube, angles, fill=fill) :
+                      collapse(derotate!(cube, angles, fill=fill); method = method)
+end
 
+ _collapse_deweighted(cube::AbstractArray{T,3}, angles::AbstractVector; fill=0) where T = 
+    _collapse_deweighted!(deepcopy(cube), angles; fill=fill)
 # deweight using Bottom et al. 2017
-function collapse!(cube::AbstractArray{T,3}, angles::AbstractVector, ::Val{true}; kwargs...)
+function _collapse_deweighted!(cube::AbstractArray{T,3}, angles::AbstractVector; fill=0) where T
+    varframe = var(cube, dims=1)
+
+    # have to check if no variance otherwise the returns will be NaN
+    all(varframe .≈ 0) && return mean(derotate!(cube, angles; fill=fill); dims=1)[1, :, :]
     # create a cube from the variance of each pixel across time
-    vc = similar(cube)
-    vm = var(cube, dims=1)
+    varcube = similar(cube)
     for idx in axes(vc, 1)
-        @inbounds vc[idx, :, :] .= vm[1, :, :]
+        @inbounds varcube[idx, :, :] .= varframe[1, :, :]
     end
     # derotate both cubes and perform a weighted sum
-    derotate!(cube, angles)
-    derotate!(vc, angles)
-    return sum(result ./ vc, dims=1)[1, :, :] ./ sum(inv.(vc), dims=1)[1, :, :]
+    derotate!(cube, angles; fill=fill)
+    derotate!(varcube, angles; fill=fill)
+    return sum(cube ./ varcube, dims=1)[1, :, :] ./ sum(inv.(varcube), dims=1)[1, :, :]
 end
 
 """
@@ -157,10 +160,10 @@ end
 
 In-place version of [`derotate`](@ref)
 """
-function derotate!(cube::AbstractArray{T,3}, angles::AbstractVector; fill=0) where {T <: AbstractFloat}
+function derotate!(cube::AbstractArray{T,3}, angles::AbstractVector; fill=0) where T
     @inbounds for i in axes(cube, 1)
         frame = @view cube[i, :, :]
-        cube[i, :, :] .= imrotate(frame, deg2rad(angles[i]), axes(frame)), Linear(), fill)
+        cube[i, :, :] .= imrotate(frame, deg2rad(angles[i]), axes(frame), Linear(), fill)
     end
     return cube
 end
