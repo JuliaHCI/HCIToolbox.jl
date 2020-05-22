236fd4c8a1b1d702b36e1326f9578b1061c44c6f
Miles Lucas <mdlucas@hawaii.edu>
2020-05-21 19:59:14 -1000

    organize kernels into a module and add docstrings

--- src/inject.jl
+++ src/inject.jl
@@ -7,29 +7,201 @@ using ImageTransformations
 using Interpolations
 using StaticArrays
 
-export Kernels, PSFModel, normalize_psf, normalize_psf!
+export Kernels, construct, normalize_psf, normalize_psf!
+
+"""
+    Kernels
+
+This module is a simple wrapper around some synthetic PSF kernels.
+
+Available kernels
+- [`Kernels.Gaussian`](@ref)/[`Kernels.Normal`](@ref)
+- [`Kernels.Moffat`](@ref)
+- [`Kernels.Airy`](@ref)
+"""
+module Kernels
+
+"""
+    Kernels.PSFKernel <: Function
+
+A kernel used for defining a PSF.
+
+# Interface
+
+Right now, each kernel has one function that returns the amplitude of the kernel given a squared Euclidean distance. The amplitude should be scaled appropriately such that the maximum value is 1.
+```julia
+(kernel::PSFKernel)(sqdist::Number)::Number
+```
+"""
+abstract type PSFKernel <: Function end
+
+"""
+    Kernels.Gaussian(fwhm)
+    Kernels.Normal(fwhm)
+
+A Gaussian kernel
+
+``K(d) = \\exp\\left[-4\\ln{2}\\left(\\frac{d}{\\Gamma}\\right)^2\\right]``
+
+```
+  ┌────────────────────────────────────────┐ 
+1 │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠊⡏⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠇⠀⡇⠘⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡜⠀⠀⡇⠀⢣⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠃⠀⠀⡇⠀⠈⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡎⠀⠀⠀⡇⠀⠀⢣⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠃⠀⠀⠀⡇⠀⠀⠘⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡜⠀⠀⠀⠀⡇⠀⠀⠀⢣⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠇⠀⠀⠀⠀⡇⠀⠀⠀⠘⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡜⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⢣⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠃⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠘⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡎⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⢱⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡸⠁⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⢇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠃⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠘⢆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠃⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠈⢆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+0 │⣀⣀⣀⣀⣀⣀⣀⣀⠤⠒⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⠢⢄⣀⣀⣀⣀⣀⣀⣀│ 
+  └────────────────────────────────────────┘ 
+  -2fwhm                               2fwhm
+```
+"""
+struct Gaussian{T} <: PSFKernel
+    fwhm::T
+end
+
+(g::Gaussian)(sqdist) = exp(-4log(2) * sqdist / g.fwhm^2)
+
+# Alias Normal -> Gaussian
+const Normal = Gaussian
+@doc (@doc Gaussian) Normal
+
+
+"""
+    Kernels.Moffat(fwhm)
+
+A Moffat kernel
+
+``K(d) = \\left[1 + \\left(\\frac{d}{\\Gamma/2}\\right)^2 \\right]^{-1}``
+
+```
+  ┌────────────────────────────────────────┐ 
+1 │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠎⡧⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡜⠀⡇⢱⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠁⠀⡇⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⡇⠀⢸⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⡇⠀⠀⢇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⡇⠀⠀⠸⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡸⠀⠀⠀⠀⡇⠀⠀⠀⢇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠇⠀⠀⠀⠀⡇⠀⠀⠀⠘⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡎⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⢱⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡎⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⢣⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡜⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠱⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠃⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠑⢆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⢀⡠⠊⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠢⣀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⢀⣀⡠⠤⠔⠊⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠒⠤⠤⣀⣀⠀│ 
+0 │⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉│ 
+  └────────────────────────────────────────┘  
+  -2fwhm                               2fwhm
+```
+"""
+struct Moffat{T} <: PSFKernel
+    fwhm::T
+end
+
+function (m::Moffat)(sqdist)
+    hwhm = m.fwhm / 2
+    return inv(1 + sqdist / hwhm^2)
+end
+
+"""
+    Kernels.AiryDisk(fwhm)
+
+An Airy disk. Guaranteed to work even at `r=0`.
+
+``K(r) = \\left[2\\frac{J_1\\left(\\pi r \\right)}{\\pi r}\\right]^2`` where ``r \\approx \\frac{d}{0.97\\Gamma}``
+
+```
+  ┌────────────────────────────────────────┐ 
+1 │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡰⠊⡏⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠃⠀⡇⠘⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡎⠀⠀⡇⠀⢱⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠁⠀⠀⡇⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡎⠀⠀⠀⡇⠀⠀⢱⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠃⠀⠀⠀⡇⠀⠀⠈⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡎⠀⠀⠀⠀⡇⠀⠀⠀⢣⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠃⠀⠀⠀⠀⡇⠀⠀⠀⠘⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡜⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⢣⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠇⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠘⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡜⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⢣⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠃⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠘⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡎⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠸⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+  │⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡜⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⢣⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀│ 
+0 │⣀⣀⣀⣀⠤⣀⣀⣀⣀⣀⠎⠁⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠣⢄⣀⣀⣀⣀⡠⢄⣀⣀⣀│ 
+  └────────────────────────────────────────┘ 
+  -2fwhm                               2fwhm
+```
+"""
+struct AiryDisk{T} <: PSFKernel
+    fwhm::T
+end
+
+const rz = 3.8317059702075125 / π
+
+function (a::AiryDisk)(sqdist::T) where T
+    radius = a.fwhm * 1.18677
+    r = sqrt(sqdist) / (radius / rz)
+    return iszero(r) ? one(T) : (2besselj1(π * r) / (π * r))^2
+end
+
+end # module Kernels
+
+using .Kernels: PSFKernel
 
 ###################
 
-abstract type PSFKernel end
+"""
+    construct(::PSFKernel, size; A=1, pa=0, location...)
+    construct(::PSFKernel, indices; A=1, pa=0, location...)
 
-construct(kernel::PSFKernel, size::Tuple{<:Integer, <:Integer}; A=1, pa=0, location...) = 
-    construct(kernel, Base.OneTo.(size); A=A, pa=pa, location...)
+Constructs a synthetic PSF using the given kernel.
 
+If `indices` are provided they will be used as the input grid. If `size` is provided the PSF will have the given size. It will have amplitude `A` and will be located at the given `location` (explained below) potentially rotated by an additional `pa` degrees clockwise.
+
+### Coordinate System
+* `x, y` - Parsed as distance from the bottom-left corner of the image. Pixel convention is that `(1, 1)` is the center of the bottom-left pixel increasing right and up.
+* `r, theta` or `r, θ` - Parsed as polar coordinates from the center of the image. `theta` is a position angle.
+"""
 function construct(kernel::PSFKernel, idxs::Tuple{<:AbstractVector, <:AbstractVector}; A=1, pa=0, location...)
+    S = mapreduce(typeof, promote_type, values(location)) |> float
     ys, xs = idxs
     ctr = _frame_center(idxs)
     x0, y0 = _get_location(ctr, values(location), pa)
-    return @. A * kernel(_sqeuclidean(x0, y0, xs', ys))
+    r2 = @. (x - x0)^2 + (y - y0)^2
+    return @. S(A * kernel(_r2))
 end
 
-function construct(kernel::AbstractMatrix{T}, idxs::Tuple{<:AbstractVector, <:AbstractVector}; A=1, pa=0, location...) where T
+"""
+    construct(::AbstractMatrix, size; A=1, pa=0, location...)
+    construct(::AbstractMatrix, indices; A=1, pa=0, location...)
+
+Constructs a PSF using the given matrix via bilinear interpolation.
+
+If `indices` are provided they will be used as the input grid. If `size` is provided the PSF will have the given size. It will have amplitude `A` and will be located at the given `location` (explained below) potentially rotated by an additional `pa` degrees clockwise.
+
+### Coordinate System
+* `x, y` - Parsed as distance from the bottom-left corner of the image. Pixel convention is that `(1, 1)` is the center of the bottom-left pixel increasing right and up.
+* `r, theta` or `r, θ` - Parsed as polar coordinates from the center of the image. `theta` is a position angle.
+"""
+function construct(kernel::AbstractMatrix, idxs::Tuple{<:AbstractVector, <:AbstractVector}; A=1, pa=0, location...)
+    S = mapreduce(typeof, promote_type, values(location)) |> float
     ctr = _frame_center(idxs)
     # have to do reversing and flip sign because `warp` moves canvas not image
     pos = _get_location(ctr, values(location), pa) |> reverse
     tform = Translation(_frame_center(kernel) - pos)
-    return A .* warp(kernel, tform, idxs, Linear(), zero(T))
+    return @. S(A * warp(kernel, tform, idxs, Linear(), zero(S)))
 end
+
+construct(kernel::PSFKernel, size::Tuple{<:Integer, <:Integer}; A=1, pa=0, location...) =
+    construct(kernel, Base.OneTo.(size); A=A, pa=pa, location...)
 
 function _frame_center(axes)
     map(axes) do axis
@@ -64,16 +236,14 @@ _get_location(ctr, pars::NamedTuple{(:θ, :r)}, pa=0) =
 ################################
 
 """
-    inject(frame, img; x, y, A=1)
-    inject(frame, img; r, theta, A=1)
+    inject(frame, ::PSFKernel; A=1, location...)
+    inject(frame, ::AbstractMatrix; A=1, location...)
 
-Injects `A * img` into `frame` at the position relative to the center of `frame` given by the keyword arguments. If necessary, `img` will be bilinearly interpolated onto the new indices. When used for injecting a PSF, it is imperative the PSF is already centered and, preferrably, odd-sized.
+Injects the given PSF kernel or image into `frame` with amplitude `A`. The location can be given in image or polar coordinates, following the [`Coordinate System`](@ref) convention.
 
 ### Coordinate System
-
-The positions are decided in this way:
 * `x, y` - Parsed as distance from the bottom-left corner of the image. Pixel convention is that `(1, 1)` is the center of the bottom-left pixel increasing right and up.
-* `r, theta` - Parsed as polar coordinates from the center of the image. `theta` is a position angle.
+* `r, theta` or `r, θ` - Parsed as polar coordinates from the center of the image. `theta` is a position angle.
 
 !!! note
     Due to the integral nature of array indices, frames or images with even-sized axes will have their center rounded to the nearest integer. This may cause unexpected results for small frames and images.
@@ -101,8 +271,8 @@ inject(frame::AbstractMatrix, kernel; A=1, location...) =
     inject!(deepcopy(frame), kernel; A=A, location...)
 
 """
-    inject!(frame, img; x, y, A=1, pa=0)
-    inject!(frame, img; r, theta, A=1, pa=0)
+    inject!(frame, ::PSFKernel; A=1, location...)
+    inject!(frame, ::AbstractMatrix; A=1, location...)
 
 In-place version of [`inject`](@ref) which modifies `frame`.
 """
@@ -111,8 +281,8 @@ function inject!(frame::AbstractMatrix, kernel; A=1, pa=0, location...)
 end
 
 """
-    inject(cube, img, [angles]; x, y, A=1)
-    inject(cube, img, [angles]; r, theta, A=1)
+    inject(cube, ::PSFKernel, [angles]; A=1, location...)
+    inject(cube, ::AbstractMatrix, [angles]; A=1, location...)
 
 Injects `A * img` into each frame of `cube` at the position given by the keyword arguments. If `angles` are provided, the position in the keyword arguments will correspond to the `img` position on the first frame of the cube, with each subsequent repositioned `img` being rotated by `-angles` in degrees. This is useful for fake companion injection.
 """
@@ -122,8 +292,8 @@ inject(cube::AbstractArray{T,3}, kernel, angles; A=1, location...) where T =
     inject!(deepcopy(cube), kernel, angles; A=A, location...)
 
 """
-    inject!(cube, img, [angles]; x, y, A=1)
-    inject!(cube, img, [angles]; r, theta, A=1)
+    inject!(cube, ::PSFKernel, [angles]; A=1, location...)
+    inject!(cube, ::AbstractMatrix, [angles]; A=1, location...)
 
 In-place version of [`inject`](@ref) which modifies `cube`.
 """
@@ -136,7 +306,7 @@ function inject!(cube::AbstractArray{T,3}, kernel; A=1, location...) where T
 end
 
 function inject!(cube::AbstractArray{T,3}, kernel, angles::AbstractVector; A=1, location...) where T
-    size(cube, 1) == length(angles) || 
+    size(cube, 1) == length(angles) ||
         error("Number of ADI frames does not much between cube and angles- got $(size(cube, 1)) and $(length(angles))")
     for idx in axes(cube, 1)
         frame = @view cube[idx, :, :]
@@ -144,41 +314,6 @@ function inject!(cube::AbstractArray{T,3}, kernel, angles::AbstractVector; A=1, 
     end
     return cube
 end
-
-###################
-
-_sqeuclidean(x0, y0, x, y) = (x - x0)^2 + (y - y0)^2
-
-struct Gaussian{T} <: PSFKernel
-    fwhm::T
-end
-
-(g::Gaussian)(sqdist) = exp(-4log(2) * sqdist / g.fwhm^2)
-
-
-struct Moffat{T} <: PSFKernel
-    fwhm::T
-end
-
-function (m::Moffat)(sqdist)
-    hwhm = m.fwhm / 2
-    α = 1
-    return (1 + sqdist / hwhm^2)^(-α)
-end
-
-struct AiryDisk{T} <: PSFKernel
-    fwhm::T
-end
-
-const rz = 3.8317059702075125 / π
-
-function (a::AiryDisk)(sqdist)
-    radius = a.fwhm * 1.18677
-    r = sqrt(sqdist) / (radius / rz)
-    return iszero(r) ? one(T) : (2besselj1(π * r) / (π * r))^2
-end
-
-const Kernels = (Gaussian=Gaussian, Normal=Gaussian, Moffat=Moffat, Airy=AiryDisk)
 
 ###################
 
