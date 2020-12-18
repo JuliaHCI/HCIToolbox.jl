30ededdb7ae773e01a5bd915c7c9075567270b0c
Miles Lucas <mdlucas@hawaii.edu>
2020-12-18 17:42:30 -0600

    fix tests

--- src/geometry/multiannulus.jl
+++ src/geometry/multiannulus.jl
@@ -16,13 +16,19 @@ function MultiAnnulusView(parent::AbstractArray{T,3}, radii, width; fill=zero(T)
         filter(idx -> inside_annulus(inner, outer, center(parent)[2:3], idx), space_indices)
     end
     indices = [(time_axis, space_axis) for space_axis in space_axes]
-    return MultiAnnulusView(parent, radii, width, indices, convert(T, fill))
+    return MultiAnnulusView(parent, radii, Float64(width), indices, convert(T, fill))
 end
 
 function MultiAnnulusView(parent::AbstractArray{T,3}, width; inner=0, outer=(last(size(parent)) + 1) / 2, fill=zero(T)) where T
     first_r = inner + width/2
-    final_r = outer - width/2
-    radii = first_r:width:final_r
+    if isfinite(outer)
+        final_r = outer - width/2
+    else
+        max_length = (last(size(parent)) + 1) / 2
+        max_r = sqrt(2 * max_length^2)
+        final_r = max_r + 1 / sqrt(2)
+    end
+    radii = range(first_r, final_r, step=width)
     return MultiAnnulusView(parent, radii, width; fill=fill)
 end
 
@@ -59,17 +65,40 @@ end
 annuli(view::MultiAnnulusView, asview=false) = (view(i, asview) for i in 1:length(view.indices))
 
 function Base.copyto!(view::MultiAnnulusView, mats::Vararg{<:AbstractMatrix})
-    inverse!(view, view.parent, mats)
+    if length(mats) != length(view.indices)
+        error("Number of matrices does not match number of annuli")
+    end
+    inverse!(view, view.parent, mats...)
+    return view
+end
+
+Base.copyto!(view::MultiAnnulusView, mats::AbstractVector{<:AbstractMatrix}) = copyto!(view, mats...)
+
+function Base.copyto!(view::MultiAnnulusView, idx::Int, mat::AbstractMatrix)
+    inverse!(view, view.parent, idx, mat)
     return view
 end
 
 function inverse!(view::MultiAnnulusView, out, mats::Vararg{<:AbstractMatrix})
+    if length(mats) != length(view.indices)
+        error("Number of matrices does not match number of annuli")
+    end
     for (idxs, mat) in zip(view.indices, mats)
         out[idxs...] = mat
     end
     return out
 end
 
+function inverse!(view::MultiAnnulusView, out, idx::Int, mat::AbstractMatrix)
+    idxs = view.indices[idx]
+    out[idxs...] = mat
+    return out
+end
+
 inverse!(view::MultiAnnulusView, out, mats) = inverse!(view, out, mats...)
-inverse(view::MultiAnnulusView, mats::Vararg{<:AbstractMatrix}) = inverse!(view, fill!(similar(parent(view)), view.fill), mats...)
+
+function inverse(view::MultiAnnulusView, args...)
+    out = fill!(similar(parent(view)), view.fill)
+    inverse!(view, out, args...)
+end
 inverse(view::MultiAnnulusView, mats) = inverse(view, mats...)
--- test/geometry.jl
+++ test/geometry.jl
@@ -32,29 +32,46 @@ end
 
 @testset "MultiAnnulusView" begin
     data = ones(10, 501, 501)
-
-    av = AnnulusView(data; inner=10, outer=30)
+    fwhm = 5
+    av = MultiAnnulusView(data, fwhm; inner=10, outer=30)
 
     @test size(av) == size(data)
-    @test 10 * π * (av.rmax^2 - av.rmin^2) ≈ sum(av) rtol=1/sqrt(sum(av))
+    @test av.radii ≈ 12.5:5:27.5
+    @test length(av.indices) == length(av.radii)
+    @test av.width ≈ fwhm
+    area = 10 * π * (30^2 - 10^2)
+    @test area ≈ sum(av) rtol=1/sqrt(count(!iszero, av))
 
-    X = av()
-    @test all(size(X) .== map(length, av.indices))
+    X = av(1)
+    @test all(size(X) .== map(length, av.indices[1]))
     @test all(isone, X)
-    sub_cube = inverse(av, -ones(size(X)))
-    @test sub_cube ≈ copyto!(copy(av), -ones(size(X)))
+    # only subs the first annulus
+    @test_throws MethodError inverse(av, -X)
+    @test_throws ErrorException copyto!(copy(av), -X)
+    sub_cube = inverse(av, 1, -X)
+    @test all(sub_cube[av.indices[1]...] .≈ -1)
+    copy_cube = copyto!(copy(av), 1, -X)
+    @test all(copy_cube[av.indices[1]...] .≈ -1)
 
-    X_view = av(true)
+    @test_throws DimensionMismatch inverse(av, 2, X)
+
+    X_view = av(1, true)
     @test X_view isa SubArray
     @test X_view ≈ X
 
-    @test -10π * (av.rmax^2 - av.rmin^2) ≈ sum(sub_cube) rtol=1/sqrt(count(!iszero, sub_cube))
+    av = MultiAnnulusView(data, fwhm; inner=10, outer=30)
+    @test HCIToolbox.annuli(av) isa Base.Generator
+    Xs = [-X for X in HCIToolbox.annuli(av)]
+    sub_cube = inverse(av, Xs)
+    @test sub_cube ≈ copyto!(copy(av), Xs)
+
+    @test -area ≈ sum(sub_cube) rtol=1/sqrt(count(!iszero, sub_cube))
 
     data = randn(rng, 10, 501, 501)
 
-    @test AnnulusView(data; inner=0, outer=Inf) ≈ data
+    @test MultiAnnulusView(data, fwhm; inner=0, outer=Inf) ≈ data
 
-    nanview = AnnulusView(data; fill=NaN)
+    nanview = MultiAnnulusView(data, fwhm; fill=NaN)
     @test_throws BoundsError nanview[-1, 2, 3]
     @test isnan(nanview[1, 1, 1])
 end
