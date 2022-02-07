811c627ec3c452488bc493486ac2913eb8a59f01
mileslucas <mdlucas@hawaii.edu>
2022-02-06 22:58:22 -1000

    update scaling and tests

--- test/scaling.jl
+++ test/scaling.jl
@@ -1,44 +1,42 @@
 
 @testset "scale" begin
     data = ones(2, 2, 3)
-    scales = [1, 2, 3]
+    scales = [3, 2, 1]
     sc_cube = scale(data, scales)
-    @test size(sc_cube) == (6, 6, 3)
-    @test all(sc_cube[3:4, 3:4, 1] .≈ 1)
-    @test all(sc_cube[2:5, 2:5, 2] .≈ 1)
-    @test all(sc_cube[3, :, :] .≈ 1)
-    @test sum(sc_cube, dims=(1, 2)) ≈ [36, 16, 4]
+    @test size(sc_cube) == size(data)
+    @test all(sc_cube .≈ 1)
 
     for i in axes(data, 3)
-        @test scale(data[:, :, i], scales[i], (6, 6)) ≈ sc_cube[:, :, i]
+        @test scale(data[:, :, i], scales[i]) ≈ sc_cube[:, :, i]
     end
 end
 
 @testset "invscale" begin
-    data = ones(6, 6, 3)
-    scales = [1, 2, 3]
+    data = ones(8, 8, 3)
+    scales = [4, 2, 1]
     sc_cube = invscale(data, scales)
-    @test size(sc_cube) == (2, 2, 3)
-    @test all(sc_cube .≈ 1)
+    @test size(sc_cube) == size(data)
+    @test all(sc_cube[4:5, 4:5, 1] .≈ 1)
+    @test all(sc_cube[3:6, 3:6, 2] .≈ 1)
+    @test all(sc_cube[:, :, 3] .≈ 1)
+    @test sum(sc_cube, dims=(1, 2)) |> vec ≈ [4, 16, 64]
 
     for i in axes(data, 3)
-        @test invscale(data[:, :, i], scales[i], (2, 2)) ≈ sc_cube[:, :, i]
+        @test invscale(data[:, :, i], scales[i]) ≈ sc_cube[:, :, i]
     end
 end
 
 @testset "stack and collapse" begin
-    data = ones(2, 2, 4, 3)
-    scales = [1, 2, 3]
+    data = ones(8, 8, 3, 4)
+    scales = [4, 2, 1]
     stack = scale_and_stack(data, scales)
-    @test size(stack) == (6, 6, 12)
-    @test all(stack[3:4, 3:4, 1:3:12] .≈ 1)
-    @test all(stack[2:5, 2:5, 2:3:12] .≈ 1)
-    @test all(stack[:, :, 3:3:12] .≈ 1)
-    @test sum(stack) ≈ 4 * sum((36, 16, 4))
+    @test size(stack) == (8, 8, 12)
+    @test all(stack .≈ 1)
+    @test sum(stack) ≈ length(data)
 
-    cube = invscale_and_collapse(stack, scales, (2, 2))
-    @test size(cube) == (2, 2, 4)
-    @test all(cube .≈ 1)
+    cube = invscale_and_collapse(stack, scales; method=sum)
+    @test size(cube) == (8, 8, 4)
+    @test sum(cube) ≈ 336
 end
 
 @testset "scale list" begin
