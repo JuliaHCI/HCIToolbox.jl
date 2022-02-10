71e4db5a1beb605e4fabc5089c419cd1492cc884
mileslucas <mdlucas@hawaii.edu>
2022-02-09 23:17:12 -1000

    add examples and docstring

--- src/profiles.jl
+++ src/profiles.jl
@@ -1,13 +1,46 @@
 using ImageTransformations: center
 using StatsBase
 
-function radial_profile(image, center=center(image))    
+"""
+    radial_profile(image; center=center(image))
+
+Calculates the radial profile of `image`, centered around `center`. Returns the radii and corresponding profile.
+
+!!! note "Understanding bin edges"
+
+    The radii returned here are integral, for example `[0, 1, 2, ...]`. These are equivalent to the centers of the contours of an annulus between `r - 0.5, r + 0.5` pixels. These annuli are centered on pixels (at least those orthogonal to the axes). 
+
+# Examples
+
+```jldoctest
+julia> X = [0 1 1 1 0
+            1 2 2 2 1
+            1 2 3 2 1
+            1 2 2 2 1
+            0 1 1 1 0];
+
+julia> r, prof = radial_profile(X);
+
+julia> r .=> prof # radius => value
+4-element Vector{Pair{Int64, Float64}}:
+ 0 => 3.0
+ 1 => 2.0
+ 2 => 1.0
+ 3 => 0.0
+
+julia> radial_profile([1 2; 2 2], center=(1, 1))
+([0, 1], [1.0, 2.0])
+```
+"""
+function radial_profile(image::AbstractMatrix; center=center(image))
     inds = CartesianIndices(image)
     distance = vec(map(idx -> round(Int, sqrt((idx.I[1] - center[1])^2 + (idx.I[2] - center[2])^2)), inds))
     weights = vec(image)
     
-    profile = sort(countmap(distance, weights))
+    binned = sort(countmap(distance, weights))
     nr = sort(countmap(distance))
     
-    return collect(keys(profile)), values(profile) ./ values(nr)
+    radii = collect(keys(binned))
+    profile = values(binned) ./ values(nr)
+    return radii, profile
 end
